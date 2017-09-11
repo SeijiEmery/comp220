04beb32cbeb72b5ecdf0af992e94c7d93902dcbd
Seiji Emery <seijiemery@gmail.com>
2017-09-10 23:53:42 -0700

    completely replaced test framework with something much simpler

--- assignment_02/src/StaticArray.TestDriver.cpp
+++ assignment_02/src/StaticArray.TestDriver.cpp
@@ -4,6 +4,26 @@
 // StaticArray.TestDriver.cpp
 // Tests our static array implementation.
 //
+// Test implementation: uses custom test macros instead of assert().
+// Uses global variables (bad but KISS) so obviously not threadsafe.
+//
+// ============================================================================
+// Note: uses ANSI escape sequences to display colored text for test results.
+// If you see funky stuff like "\033[32mAll tests passed\n\033[0m", run the program in a
+// terminal / console that supports this feature: https://en.wikipedia.org/wiki/ANSI_escape_code
+//
+// Xcode does not, but you can run it properly with the following steps:
+//  * click Products folder > right click <target executable>
+//  * click show in finder
+//  * double click on <target executable> to run in terminal
+//
+// I'm not sure whether this will work on windows (it should in some terminals but might not 
+// work in CMD.exe (haven't tested); visual studio may be in the same situation as xcode)
+// ============================================================================
+//
+// Remote source:
+// https://github.com/SeijiEmery/comp220/tree/master/assignment_02/src/StaticArray.TestDriver.cpp
+//
 
 #include <iostream>     // cerr, cout
 #include <string>       // string
@@ -28,73 +48,41 @@ int main () {
     TEST_ARRAY_IMPL(double, 1.5, 2.5, 3.5);
     TEST_ARRAY_IMPL(char, '@', 'Z', 'a');
     TEST_ARRAY_IMPL(std::string, "bar", "baz", "foo");
-    std::cout << "All tests passed\n";
+    std::cout << "\033[32mAll tests passed\n\033[0m";
     return 0;
 }
 
 //
-// Mini-testing framework (©2017 Seiji Emery)
-//
-// Note: super-minimalistic, does not count tests passed / failed, only one test macro (variants of REQUIRE()),
-// calls exit(-1) when a test fails. Tests can be organized via SECTION().
-//
-// Intended to be a quick-and-dirty, <100 LOC replacement for catch.hpp for very small projects.
+// Minimalistic test 'framework' for comp220. Extremely simple, etc.
 //
 
-#include <vector>       // vector
-#include <functional>   // function
-#include <cstdlib>      // exit
+struct TestInfo {
+    unsigned passed = 0, failed = 0;
+} testcase;
 
-// Setup unittest dependencies
-namespace detail {
-    // Used by SECTION(): stack of closures that print out what sections we're inside of.
-    std::vector<std::function<void()>> g_testScope;
+#define ASSERT_EQ(a,b) do {\
+    if (a == b) ++testcase.passed, std::cout << "\033[32mPASS\033[0m: " #a " == " #b " (file " __FILE__ ":" << __LINE__ << ")\n";\
+    else        ++testcase.failed, std::cout << "\033[31mFAIL\033[0m: " #a " == " #b " (file " __FILE__ ":" << __LINE__ << ")\n";\
+    std::cout << "    EXPECTED: " #a " = '" << a << "'\n";\
+    std::cout << "    GOT:      " #b " = '" << b << "'\n";\
+} while(0)
 
-    // Helper struct for SECTION() (ctor: pushed value to g_testScope, dtor: pops value from g_testScope)
-    struct TestScopeInserter {
-        TestScopeInserter (std::function<void()> scope) { /*std::cerr << "begin scope: "; scope();*/ g_testScope.push_back(scope); }
-        ~TestScopeInserter () { /*std::cerr << "end scope: "; g_testScope.back();*/ g_testScope.pop_back(); }
-        operator bool () const { return true; }
-    };
+#define ASSERT_NE(a,b) do {\
+    if (a != b) ++testcase.passed, std::cout << "\033[32mPASS\033[0m: " #a " != " #b " (file " __FILE__ ":" << __LINE__ << ")\n";\
+    else        ++testcase.failed, std::cout << "\033[31mFAIL\033[0m: " #a " != " #b " (file " __FILE__ ":" << __LINE__ << ")\n";\
+    std::cout << "    EXPECTED: " #a " = '" << a << "'\n";\
+    std::cout << "    GOT:      " #b " = '" << b << "'\n";\
+} while(0)
 
-    // Helper function for REQURE_**(): prints a message, displays section stack, and exits (-1).
-    template <typename A, typename B>
-    void signalAssertFailure (const char* msg, const A& a, const B& b) {
-        std::cerr << msg << " (" << a << ", " << b << "), file " << __FILE__ << ':' << __LINE__ << '\n';
-        for (auto i = g_testScope.size(); i --> 0; ) {
-            g_testScope[i]();
-        }
-        std::cerr.flush();
+#define SECTION(msg...) if ((std::cout << "\n\033[33m" << msg << "\033[0m\n"), true)
+
+static void reportTestResults () {
+    std::cout << (testcase.failed ? "\033[31m" : "\033[32m") << testcase.passed << " / " << (testcase.passed + testcase.failed) << " tests passed\033[0m\n\n";
+    if (testcase.failed != 0) {
         exit(-1);
     }
-}; // namespace detail
-
-// Enters a new test section, takes args that are piped to std::cerr (so SECTION("foo = " << foo()) { ... } is valid). 
-// Displays message iff test in enclosed section fails; otherwise is silent.
-#define SECTION(args...) if (auto _  = detail::TestScopeInserter([=](){ \
-    std::cerr << "in section " << args << '\n'; \
-}))
-
-// Used to implement require macros: tests that expr is true, and if not, prints out
-// stringified expr, values of a and b (presumably arguments to expr), and calls signalAssertFailure.
-#define REQUIRE_THAT(expr, a, b) do { \
-    if (!(expr)) { \
-        detail::signalAssertFailure("Test failed: " #expr, a, b); \
-    } \
-} while (0)
-
-// Standard REQUIRE / ASSERT test macros. Not using assert() b/c we want more control over output
-// on failure, and including it would be redundant. Split into REQUIRE_EQ, REQUIRE_NE, etc., b/c
-// this is the simplest way to implement a REQUIRE() macro that can stringify args on test failure
-#define REQUIRE_EQ(a,b) REQUIRE_THAT(a == b, a, b)
-#define REQUIRE_NE(a,b) REQUIRE_THAT(a != b, a, b)
-#define REQUIRE_GE(a,b) REQUIRE_THAT(a >= b, a, b)
-#define REQUIRE_LE(a,b) REQUIRE_THAT(a <= b, a, b)
-#define REQUIRE_GT(a,b) REQUIRE_THAT(a > b, a, b)
-#define REQUIRE_LT(a,b) REQUIRE_THAT(a < b, a, b)
-//
-// End mini-testing framework
-//
+    testcase.failed = testcase.passed = 0;
+}
 
 
 //
@@ -104,49 +92,58 @@ template <typename T, size_t N>
 void _testArrayImpl (const char* name, T init, T first, T second, T third) {
     SECTION("Testing StaticArray<" << name << ", " << N << ">") {
         SECTION("Sanity check") {
-            REQUIRE_EQ(first, first);
-            REQUIRE_GE(first, first);
-            REQUIRE_LE(first, first);
-            REQUIRE_NE(first, second);
-            REQUIRE_LT(first, second);
-            REQUIRE_GT(second, first);            
+            // ASSERT_NE(first, first);     // To verify that test framework is working, try uncommenting this line (should fail).
+            ASSERT_EQ(first, first);
+            ASSERT_NE(first, second);   
         }
         StaticArray<T,N> array;
 
         SECTION("Testing StaticArray capacity (should equal " << N << ")") {
-            REQUIRE_EQ(array.capacity(), N);
+            ASSERT_EQ(array.capacity(), N);
         }
-        SECTION("Testing StaticArray initial values (should equal " << init << ")") {
+        SECTION("Testing StaticArray initial values (should be default-initialized, equal '" << init << "')") {
+            int numNonEqualElements = 0;
             for (auto i = 0; i < array.capacity(); ++i) {
-                REQUIRE_EQ(array[i], init);
+                if (array[i] != init) ++numNonEqualElements;
             }
+            ASSERT_EQ(numNonEqualElements, 0);
         }
         SECTION("Testing StaticArray getter / setter") {
             array[0] = first;
-            REQUIRE_EQ(array[0], first);
+            ASSERT_EQ(array[0], first);
 
             array[13] = second;
-            REQUIRE_EQ(array[13], second);
+            ASSERT_EQ(array[13], second);
 
             array[N-1] = third;
-            REQUIRE_EQ(array[N-1], third);
+            ASSERT_EQ(array[N-1], third);
         }
         SECTION("Testing out-of-bounds array values") {
-            array[-1] = first;
-            REQUIRE_EQ(array[-1], first);
-            REQUIRE_EQ(array[N],  first);
-            REQUIRE_NE(array[N], array[N-1]);
+            auto* ptr = &(array[-1] = first);
+
+            ASSERT_EQ(*ptr, first);
+            ASSERT_EQ(array[-1], init);
+            ASSERT_EQ(&(array[N]), &(array[-1]));
+            ASSERT_EQ(array[N], array[-1]);
+            ASSERT_EQ(array[N], init);
+
+            // REQUIRE_EQ(array[-1], first);
+            // REQUIRE_EQ(array[N],  first);
+            ASSERT_NE(array[N], array[N-1]);
         }
 
         SECTION("Const-object test") {
             const StaticArray<T, N> array2 = array;
-            REQUIRE_EQ(array2[0], first);
-            REQUIRE_EQ(array[13], second);
-            REQUIRE_EQ(array[N-1], third);
+            ASSERT_EQ(array2[0], first);
+            ASSERT_EQ(array[13], second);
+            ASSERT_EQ(array[N-1], third);
 
+            int numNonEqualElements = 0;            
             for (auto i = 0; i < array.capacity(); ++i) {
-                REQUIRE_EQ(array[i], array2[i]);
+                if (array[i] != array2[i]) ++numNonEqualElements;
             }
+            ASSERT_EQ(numNonEqualElements, 0);
         }
     }
+    reportTestResults();
 }
