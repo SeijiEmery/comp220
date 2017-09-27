620f6f681071d50b7cb16f434d66be2e1c428c6f
Seiji Emery <seijiemery@gmail.com>
2017-09-26 21:02:18 -0700

    updated python reference program

--- assignment_04/src/dvc.py
+++ assignment_04/src/dvc.py
@@ -1,13 +1,34 @@
+duplicates = open('duplicates.py.txt', 'w')
 
+lcount, dcount = 0, 0
 subject_counts = {}
+unique_codes   = set()
+
 with open('../data/dvc-schedule.txt', 'r') as f:
-    for line in set([ line.strip() for line in f ]):
-        subject = line.split('\t')[2].split('-')[0]
-        if subject in subject_counts:
-            subject_counts[subject] += 1
+    for line in f:
+        fields = line.strip().split('\t')
+        unique_code = fields[0] + fields[1]
+        subject     = fields[2].split('-')[0]
+
+        if unique_code in unique_codes:
+            duplicates.write("DUPLICATE: '%s'\n"%line.strip())
         else:
-            subject_counts[subject] = 1
+            unique_codes.add(unique_code)
+            if subject in subject_counts:
+                subject_counts[subject] += 1
+            else:
+                subject_counts[subject] = 1
+    # for line in set([ line.strip() for line in f ]):
+    #     subject = line.split('\t')[2].split('-')[0]
+    #     if subject in subject_counts:
+    #         subject_counts[subject] += 1
+    #     else:
+    #         subject_counts[subject] = 1
+
+duplicates.close()
 
 print('\n'.join([ '%s: %s'%(k, v) for k, v in 
     sorted(subject_counts.iteritems(), key = lambda (k,v): k)
 ]))
+print("%s lines, %s duplicates"%(lcount, dcount))
+
