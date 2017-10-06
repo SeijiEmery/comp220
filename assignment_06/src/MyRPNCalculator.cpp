d4fb35764f5fb94675309d2f17a8e4424ad04a01
Seiji Emery <seijiemery@gmail.com>
2017-10-05 23:29:27 -0700

    added memory tracing + "mem" command

--- assignment_06/src/MyRPNCalculator.cpp
+++ assignment_06/src/MyRPNCalculator.cpp
@@ -13,6 +13,70 @@ using namespace std;
 #include <cmath>
 #include "Stack.h"
 
+//
+// Memory + time benchmarking code: this hijacks (overloads) global new / delete
+// to trace memory allocations (very simple: # of allocations / frees + # bytes
+// allocated / freed), and adds a global variable that displays this stuff
+// from its dtor (guaranteed to be called after main() but before program exits).
+//
+// It also adds basic time profiling (global ctor / dtor) using std::chrono.
+//
+// All of this can be achieved externally ofc using time + valgrind (*nix),
+// and is perhaps preferable - but implementing these interally was an interesting
+// exercise nevertheless.
+//
+// This can all be disabled if compiling with -D NO_MEM_DEBUG.
+//
+#ifndef NO_MEM_DEBUG
+#include <chrono>
+
+struct MemTracer {
+    void traceAlloc (size_t bytes) { ++numAllocations; allocatedMem += bytes; }
+    void traceFreed (size_t bytes) { ++numFrees; freedMem += bytes;}
+private:
+    size_t numAllocations = 0;  // number of allocations in this program
+    size_t numFrees       = 0;  // number of deallocations in this program
+    size_t allocatedMem   = 0;  // bytes allocated
+    size_t freedMem       = 0;  // bytes freed
+
+    std::chrono::high_resolution_clock::time_point t0;  // time at program start
+public: 
+    void memstat () {
+        std::cout << "\nUsed  memory: " << ((double)allocatedMem) * 1e-6 << " MB (" << numAllocations << " allocations)\n";
+        std::cout << "Freed memory: "   << ((double)freedMem)     * 1e-6 << " MB (" << numFrees       << " deallocations)\n";
+        numAllocations = 0;
+        numFrees = 0;
+        allocatedMem = 0;
+        freedMem = 0;
+    }
+
+    MemTracer () : t0(std::chrono::high_resolution_clock::now()) {}
+    ~MemTracer () {
+        using namespace std::chrono;
+        auto t1 = high_resolution_clock::now();
+        std::cout << "\nUsed  memory: " << ((double)allocatedMem) * 1e-6 << " MB (" << numAllocations << " allocations)\n";
+        std::cout << "Freed memory: "   << ((double)freedMem)     * 1e-6 << " MB (" << numFrees       << " deallocations)\n";
+        std::cout << "Ran in " << duration_cast<duration<double>>(t1 - t0).count() * 1e3 << " ms\n";
+    }
+} g_memTracer;
+
+void* operator new (size_t size) throw(std::bad_alloc) {
+    g_memTracer.traceAlloc(size);
+    size_t* mem = (size_t*)std::malloc(size + sizeof(size_t));
+    if (!mem) {
+        throw std::bad_alloc();
+    }
+    mem[0] = size;
+    return (void*)(&mem[1]);
+}
+void operator delete (void* mem) throw() {
+    auto ptr = &((size_t*)mem)[-1];
+    g_memTracer.traceFreed(ptr[0]);
+    std::free(ptr);
+}
+
+#endif // NO_MEM_DEBUG
+
 template <typename T>
 T popBack (LinkedListStack<T>& stack, T default_ = T()) {
     T back = stack.empty() ? default_ : stack.peek();
@@ -29,7 +93,7 @@ int main () {
     std::string line, input;
     bool        running = true;
 
-    std::regex  expr { "\\s*([0-9\\.]+|[\\+\\-\\*\\^\\/qQ]|help|drop|dup|disp|swap|clear|top|pi|sin|cos|tan|asin|acos|atan|sqrt|abs)\\s*" };
+    std::regex  expr { "\\s*([0-9\\.]+|[\\+\\-\\*\\^\\/qQ]|help|drop|dup|disp|swap|clear|top|pi|sin|cos|tan|asin|acos|atan|sqrt|abs|mem)\\s*" };
     std::smatch match;
     double a, b;
 
@@ -102,6 +166,11 @@ int main () {
                     if (token == "pi") {
                         // clever way to calculate pi – from https://stackoverflow.com/a/1727886
                         values.push(atan(1) * 4);
+                    }
+                break;
+                case 'm': 
+                    if (token == "mem") {
+                        g_memTracer.memstat();
                     }
                 break;
                 case 'h': if (token == "help") {
