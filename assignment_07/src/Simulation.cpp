7dbeb72ec14c10a34cc7c8d446823f58f02c3209
Seiji Emery <seijiemery@gmail.com>
2017-10-18 16:04:21 -0700

    added server config (no file I/O yet)

--- assignment_07/src/Simulation.cpp
+++ assignment_07/src/Simulation.cpp
@@ -10,19 +10,45 @@
 // remote source: https://github.com/SeijiEmery/comp220/blob/master/assignment_07/src/Simulation.cpp
 //
 
-#include <iostream>
-#include <string>
+#include <iostream> // std::cout, std::cerr
+#include <iomanip>  // std::setw
+#include <fstream>  // std::ifstream
+#include <string>   // std::string
 using namespace std;
 
 #include <cstdlib>
 #include <cmath>
 #include "Queue.h"
 
+struct ServerConfig {
+    size_t count          = 4;   // number of servers
+    double arrivalRate    = 5.5; // customer arrival rate
+    size_t maxQueueLength = 10;  // maximum wait queue length
+    size_t minServiceTime = 1;   // min service time, in minutes
+    size_t maxServiceTime = 10;  // max service time, in minutes
+    size_t arrivalEndTime = 60;  // time at which customers stop arriving, in minutes
+
+    ServerConfig () {}
+
+    friend std::ostream& operator<< (std::ostream& os, const ServerConfig& config) {
+        constexpr size_t indent = 2;
+        return os << "number of servers: " << std::setw(indent+4) << config.count << '\n'
+            << "customer arrival rate: " << std::setw(indent) << config.arrivalRate 
+            << " per minute, for " << config.arrivalEndTime << " minutes" << '\n'
+            << "maximum queue length: " << std::setw(indent+1) << config.maxQueueLength << '\n'
+            << "minimum service time: " << std::setw(indent+1) << config.minServiceTime << '\n'
+            << "maximum service time: " << std::setw(indent+1) << config.maxServiceTime << '\n';
+    }
+};
+
+
 int main () {
     std::cout << "Programmer:       Seiji Emery\n";
     std::cout << "Programmer's ID:  M00202623\n";
     std::cout << "File:             " << __FILE__ << '\n' << std::endl;
 
+    ServerConfig config;
+    std::cout << config;
     return 0;
 }
 
