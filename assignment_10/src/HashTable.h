a56112ba131b79b6523b9e5c0271a6261dda9f58
Seiji Emery <seijiemery@gmail.com>
2017-11-12 23:50:46 -0800

    added interactive program; debugging, work in progress

--- assignment_10/src/HashTable.h
+++ assignment_10/src/HashTable.h
@@ -1,8 +1,56 @@
 #ifndef HashTable_h
 #define HashTable_h
 
-template <typename Key, typename Value>
+#include <utility>      // std::pair
+#include <algorithm>    // std::fill
+#include <cassert>      // assert
+
+#include <iostream>
+
+#define SET_COLOR(code) "\033[" code "m"
+#define CLEAR_COLOR SET_COLOR("0")
+#define SET_CYAN    SET_COLOR("36;1")
+#define SET_RED     SET_COLOR("31;1")
+#define SET_GREEN   SET_COLOR("32;1")
+#define SET_YELLOW  SET_COLOR("33;1")
+#define SET_BLUE    SET_COLOR("34;1")
+#define SET_PINK    SET_COLOR("35;1")
+
+struct LineWriter {
+    std::ostream& os;
+    bool shouldClearColor;
+    LineWriter (std::ostream& os, const char* startColor = nullptr) : 
+        os(os), shouldClearColor(startColor != nullptr)
+    {
+        if (startColor) { os << startColor; }
+    }
+    template <typename T>
+    LineWriter& operator<< (const T& other) {
+        return os << other, *this;
+    }
+    ~LineWriter () {
+        if (shouldClearColor) { os << CLEAR_COLOR "\n"; }
+        else { os << '\n'; }
+    }
+};
+LineWriter writeln  (std::ostream& os, const char* color = nullptr) { return LineWriter(os, color); }
+LineWriter writeln  (const char* color = nullptr) { return LineWriter(std::cout, color); }
+LineWriter report () { return writeln(SET_CYAN); }
+LineWriter warn   () { return writeln(SET_RED); }
+LineWriter info   () { return writeln(SET_GREEN); }
+
+
+const static char* colors[11] = { SET_COLOR("31"), SET_COLOR("32"), SET_COLOR("33"), SET_COLOR("34"), SET_COLOR("35"), SET_COLOR("1;30"), SET_COLOR("1;31"), SET_COLOR("1;32"), SET_COLOR("1;33"), SET_COLOR("1;34"), SET_COLOR("1;35") };
+static int nextColor = 0;
+const char* getCyclingColor () { auto color = colors[nextColor]; ++nextColor; nextColor %= (sizeof(colors) / sizeof(colors[0])); return color; }
+
+
+template <typename Key, typename Value, typename HashFunction>
 class HashTable {
+public:
+    typedef HashTable<Key, Value, HashFunction> This;
+    typedef std::pair<Key, Value>               KeyValue;
+private:
     // Can't use DynamicArray (or, hence, my Bitset impl)
     // Note: this is really stupid, b/c we have to reimplement everything from scratch -_-
     // Note: this is likely to INCREASE bugs, b/c the previous code was well tested...
@@ -19,9 +67,9 @@ class HashTable {
         Bitset& operator= (Bitset&& other) { return std::swap(data, other.data), std::swap(size, other.size), *this; }
 
         static size_t allocationSize (size_t count) {
-            return (count + sizeof(size_t) - 1) / sizeof(size_t);
+            return (count + (sizeof(size_t) * 8) - 1) / (sizeof(size_t) * 8);
         }
-        void clear () { if (data) { std::fill(&data[0], &data[size / sizeof(size_t)], 0); } }
+        void clear () { if (data) { std::fill(&data[0], &data[size], 0); } }
         void assign (size_t* data, size_t size) {
             this->data = data;
             this->size = size;
@@ -32,42 +80,113 @@ class HashTable {
         bool get   (size_t index) const { return data[index / sizeof(size_t)] & (1 << (index % sizeof(size_t))); }
         void set   (size_t index) const { data[index / sizeof(size_t)] |= (1 << (index % sizeof(size_t))); }
         void clear (size_t index) const { data[index / sizeof(size_t)] &= ~(1 << (index % sizeof(size_t))); }
+
+        friend std::ostream& operator<< (std::ostream& os, const Bitset& bitset) {
+            os << bitset.size << ":";
+            for (size_t i = 0; i < bitset.size; ++i) {
+                for (size_t mask = 1; mask != 0; mask <<= 1) {
+                    os << ((bitset.data[i] & mask) ? '1' : '0');
+                }
+            }
+            return os;
+        }
     };
 
+    HashFunction hashFunction;
     size_t      capacity = 0;           // # max key / value pairs
+    size_t      capacityThreshold = 0;  // 
     size_t      count = 0;              // # non-empty key / value pairs
     size_t*     data = nullptr;         // owning pointer for all data elements
     Bitset      bitset;                 // points into data
     KeyValue*   elements = nullptr;     // points into data
+
+    const char* color;
+    LineWriter info () const { return writeln(std::cout, color); }
 public:
-    typedef std::pair<Key, Value> KeyValue;
+    friend std::ostream& operator<< (std::ostream& os, const This& self) {
+        return os << "HashTable { capacity = " << self.capacity 
+            << ", size = " << self.count 
+            << ", bitset = " << self.bitset << " }"; 
+    }
+
 
     HashTable () = delete;
-    HashTable (size_t capacity) : capacity(capacity = capacity || 128) {
+    HashTable (HashFunction hashFunction, size_t capacity = 0)
+        : hashFunction(hashFunction)
+        , capacity(capacity = capacity ? capacity : 128)
+        , capacityThreshold((size_t)(capacity * 0.75))
+        , color(getCyclingColor())
+    {
+        info() << "Allocated " << capacity;
         assert(capacity != 0);
-        size_t size = Bitset::allocationSize(capacity) + (sizeof(KeyValue) * capacity + sizeof(size_t) - 1) / sizeof(size_t);
-        data = new size_t[size];
-        bitset.assign(mem);
-        elements = reinterpret_cast<KeyValue*>(&mem[Bitset::allocationSize(capacity)]);
+        size_t bitsetSize = Bitset::allocationSize(capacity);
+        size_t elementSize = (sizeof(KeyValue) * capacity + sizeof(size_t) - 1) / sizeof(size_t);
+
+        info() << "bitset size: " << bitsetSize;
+        info() << "element size: " << elementSize 
+            << " (" << capacity << " * " << sizeof(KeyValue) << " / " << sizeof(size_t) << ")";
+
+        data = new size_t[bitsetSize + elementSize + 1000];
+        info() << "allocated " << (bitsetSize + elementSize) * sizeof(size_t) << " bytes";
+        data[bitsetSize + elementSize - 1] = 0;
+
+        bitset.assign(data, bitsetSize);
+        elements = reinterpret_cast<KeyValue*>(&data[bitsetSize]);
+        info() << "elements offset: " << reinterpret_cast<size_t>(elements) - reinterpret_cast<size_t>(data);
+        info() << "back offset: " << reinterpret_cast<size_t>(&elements[capacity-1]) - reinterpret_cast<size_t>(data);
+        elements[capacity-1] = {};
     }
-    HashTable (const This& other) { insert(other.begin(), other.end()); }
+    HashTable (const This& other) 
+        : HashTable(other.hashFunction, other.capacity)
+    { 
+        assert(data != nullptr);
+        info() << "copying " << other.count << " elements";
+        insert(other.begin(), other.end()); 
+    }
     This& operator= (const This& other) { HashTable temp(other); std::swap(*this, temp); return *this; }
-    HashTable (This&& other) { *this = std::move(other); }
+    HashTable (This&& other)
+        : HashTable(other.hashFunction, other.capacity)
+    { 
+        assert(data != nullptr);
+        *this = std::move(other); 
+    }
     This& operator= (This&& other) {
+        info() << "Swapping " << capacity;
+        other.info() << "with " << other.capacity;
+
+        std::swap(hashFunction, other.hashFunction);
         std::swap(capacity, other.capacity);
+        std::swap(capacityThreshold, other.capacityThreshold);
         std::swap(count, other.count);
         std::swap(data, other.data);
         std::swap(bitset, other.bitset);
         std::swap(elements, other.elements);
+        std::swap(color, other.color);
+        return *this;
     }
-    ~HashTable () { clear(); delete[] data; }
+    ~HashTable () { 
+        assert(data != nullptr);
+        info() << "Destructing " << capacity;
+        clear();
+        delete[] data;
+    }
 
     size_t size () const { return count; }
     void resize (size_t size) {
+        info() << "Resizing " << capacity << " => " << size << " (has " << count << " elements)";
         assert((size > count) && "Not enough capacity to hold all data elements!");
-        HashTable temp(size);
+        HashTable temp(hashFunction, size);
         temp.insert(begin(), end());
         *this = std::move(temp);
+    }
+    void debugMemLayout () {
+        for (size_t i = 0; i < capacity; ++i) {
+            if (bitset.get(i)) {
+                info() << "    " << i << ": " << elements[i].first << " => " << elements[i].second;
+            } else {
+                info() << "    " << i << ": --";
+            }
+        }
     }
     // Reinsert all elements
     void reinsert () { 
@@ -76,6 +195,8 @@ public:
     // Clear all elements
     void clear () {
         if (count != 0) {
+            info() << "Clearing " << capacity;
+
             // Fire destructors on all non-empty elements
             for (auto& kv : *this) {
                 kv.first.~Key();
@@ -84,19 +205,23 @@ public:
             // Clear size + usage bitmask
             count = 0;
             bitset.clear();
+        } else {
+            info() << "Already cleared " << capacity;
         }
     }
 private:
     size_t locate (const Key& key) const {
-        size_t hash = hashFunction(key) % capacity;
+        size_t hash = hashFunction(key) % capacity; size_t iterations = 0;
         while (bitset.get(hash) && elements[hash].first != key) {
             hash = (hash + 1) % capacity;
+            ++iterations;
         }
+        info() << "Found hash for key " << key << ": " << hash << ", distance " << iterations << " (capacity " << capacity << ")";
         return hash;
     }
 public:
     const Value& operator[] (const Key& key) const {
-        return elements[locate(key)];
+        return elements[locate(key)].second;
     }
     Value& operator[] (const Key& key) {
         auto index = locate(key);
@@ -105,18 +230,20 @@ public:
             bitset.set(index);
             elements[index] = { key, {} };
             if (count > capacityThreshold) {
+                info() << "Capacity exceeded: " << count << " > " << capacityThreshold;
                 resize(capacity * 2);
             }
         }
-        return elements[index];
+        return elements[index].second;
     }
     void insert (const KeyValue& kv) {
-        auto index = locate(key);
+        auto index = locate(kv.first);
         elements[index] = kv;
         if (!bitset.get(index)) {
             ++count;
             bitset.set(index);
             if (count > capacityThreshold) {
+                info() << "Capacity exceeded: " << count << " > " << capacityThreshold;
                 resize(capacity * 2);
             }
         }
@@ -137,6 +264,7 @@ public:
     }
     template <typename It>
     void insert (It begin, It end) {
+        info() << "Inserting values";
         for (; begin != end; ++begin) {
             insert(*begin);
         }
@@ -144,36 +272,58 @@ public:
 private:
     template <typename T>
     class Iterator {
-        Hashtable& hashtable;
-        size_t     index = 0;
+        friend class HashTable;
+        HashTable<Key,Value,HashFunction>& hashtable;
+        size_t  index = 0;
 
-        Iterator (Hashtable& hashtable, size_t index = 0)
-            : hashtable(hashtable), index(index) {}
+        LineWriter info () const { return hashtable.info(); }
+
+        Iterator (decltype(hashtable)& hashtable, size_t index = 0)
+            : hashtable(hashtable), index(index) 
+        {
+            info() << "Constructed iterator: " << index;
+        }
+        void advanceToNextNonEmptyElement () {
+            auto start = index;
+            while (index < hashtable.capacity && !hashtable.bitset.get(index)) {
+                ++index;
+            }
+            if (start != index) {
+                info() << "Advanced index " << start << " => " << index;
+            }
+        }
+
     public:
-        Iterator (const Iterator<T>& other) : hashtable(other.hashtable), index(index) {}
+        Iterator (const Iterator<T>& other) : hashtable(other.hashtable), index(other.index) {
+            info() << "Copying iterator";
+            advanceToNextNonEmptyElement();
+        }
         Iterator& operator= (const Iterator<T>& other) { hashtable = other.hashtable; index = other.index; return *this; }
         operator bool () const { return index < hashtable.capacity; }
 
-        template <typename T2> bool operator== (const Iterator<T2>& other) const { return hashtable == other.hashtable && index == other.index; } 
-        template <typename T2> bool operator!= (const Iterator<T2>& other) const { return hashtable != other.hashtable || index != other.index; }
+        template <typename T2> bool operator== (const Iterator<T2>& other) const { return &hashtable == &(other.hashtable) && index == other.index; } 
+        template <typename T2> bool operator!= (const Iterator<T2>& other) const { return &hashtable != &(other.hashtable) || index != other.index; }
 
         Iterator<T>& operator++ () {
-            if (i < hashtable.count) {
-                for (++i; i < hashtable.count && !hashtable.bitset.set(i); ++i);
+            if (index < hashtable.capacity) {
+                ++index;
+                advanceToNextNonEmptyElement();
+            } else {
+                info() << "Hit end: " << index;
             }
             return *this; 
         }
-        T& operator*  () const { return hashtable.elements[i]; }
-        T* operator-> () const { return hashtable.elements[i]; }
+        T& operator*  () const { info() << "Fetching value at index " << index << ": " << hashtable.elements[index].first << ", " << hashtable.elements[index].second; return hashtable.elements[index]; }
+        T* operator-> () const { info() << "Fetching value at index " << index << ": " << hashtable.elements[index].first << ", " << hashtable.elements[index].second; return hashtable.elements[index]; }
     };
 public:
     typedef Iterator<KeyValue> iterator;
     typedef Iterator<const KeyValue> const_iterator;
 
-    iterator begin () { return iterator(*this, 0); }
-    iterator end   () { return iterator(*this, capacity); }
-    const_iterator begin () const { return const_iterator(*this, 0); }
-    const_iterator end   () const { return const_iterator(*this, capacity); }
+    iterator begin () { info() << "begin()"; return iterator(*this, 0); }
+    iterator end   () { info() << "end()"; return iterator(*this, capacity); }
+    const_iterator begin () const { info() << "cbegin()"; return const_iterator(const_cast<This&>(*this), 0); }
+    const_iterator end   () const { info() << "cend()";   return const_iterator(const_cast<This&>(*this), capacity); }
 
     iterator find (const Key& key) {
         auto index = locate(key);
@@ -188,5 +338,12 @@ public:
             end();
     }
 };
+
+template <typename Key, typename Value, typename HashFunction>
+auto make_hashtable (HashFunction hashFunction, size_t size = 0) -> HashTable<Key,Value,HashFunction> {
+    return { hashFunction, size };
+}
+
+
 
 #endif // HashTable_h
---
+++ assignment_10/src/HashTableInteractiveTest.cpp
@@ -0,0 +1,281 @@
+// Programmer: Seiji Emery
+// Programmer ID: M00202623
+//
+// TestKV.cpp
+// Implements a set of interactive tests for AssociativeArray by implementing
+// an interactive REPL w/ a simple regex-based DSL for manipulating a single
+// AssociativeArray<string, string>.
+//
+// Remote source:
+// https://github.com/SeijiEmery/comp220/tree/master/assignment_09/src/KVTest.hpp
+//
+
+
+#include <iostream>     // std::cout, std::cin
+#include <sstream>      // std::stringstream
+#include <string>       // std::string
+#include <regex>        // std::regex, std::regex_search, std::smatch
+#include <vector>       // std::pair
+#include <utility>      // std::pair
+#include <functional>   // std::function
+#include <cstdlib>  // atoi, exit
+#include "HashTable.h"
+
+
+
+
+struct SimpleRegexParser {
+    typedef std::function<void(const std::smatch&)> callback_t;
+    typedef std::pair<std::regex, callback_t> case_t;
+    std::vector<case_t> cases;
+public:
+    SimpleRegexParser () {}
+    SimpleRegexParser (std::initializer_list<case_t> cases) 
+        : cases(cases) {}
+
+private:
+    static std::string replace_whitespace (std::string input) {
+        static std::regex r { "([ ]+)"};
+        return std::regex_replace(input, r, "\\s*");
+    }
+    static std::string replace_group (std::string input) {
+        static std::regex r { "\\{[^\\}]*\\}" };
+        return std::regex_replace(input, r, "(\\w+)");
+    }
+    static std::string escape_chars (std::string input) {
+        static std::regex r { "([\\.\\+\\=\\[\\]\\(\\)\\-\\?\\:])" };
+        return std::regex_replace(input, r, ("\\$1"));
+    }
+    static std::string to_regex (std::string input) {
+        return "^\\s*" + replace_group(replace_whitespace(escape_chars(input)));
+    }
+public:
+    SimpleRegexParser& caseOf (const char* pattern, callback_t callback) {
+        auto regex = to_regex(pattern);
+        info() << "Creating case from " << pattern;
+        info() << "Generated regex:   " << regex;
+        cases.emplace_back(std::regex(regex), callback);
+        return *this;
+    }
+    bool parse (std::string& line, std::smatch &match) const {
+        for (auto& case_ : cases) {
+            if (std::regex_search(line, match, case_.first)) {
+                case_.second(match);
+                line = match.suffix().str();
+                return true;
+            }
+        }
+        return false;
+    }
+    void parse (std::istream& is) const {
+        std::string line;
+        std::smatch match;
+        while (is) {
+            while (!getline(is, line));
+            while (parse(line, match));
+        }
+    }
+};
+
+//
+// Convert iterators into ranges
+//
+
+template <typename It>
+class RSequence {
+    It begin, end;
+public:
+    RSequence (It begin, It end) : begin(begin), end(end) { info() << "constructed sequence()"; }
+    operator bool () const { return begin != end; }
+    RSequence& operator++ () { return ++begin, *this; }
+    auto operator* () const -> decltype(*begin) { return *begin; }
+};
+template <typename It>
+auto sequence (It begin, It end) -> RSequence<It> { return { begin, end }; }
+
+
+//
+// Map
+//
+
+template <typename F, typename Range>
+class RMap {
+    F f;
+    Range range;
+public:
+    RMap (F f, Range range) : f(f), range(range) { info() << "constructed map()"; }
+    operator bool () const { return bool(range); }
+    RMap& operator++ () { return ++range, *this; }
+    auto  operator*  () const -> decltype(f(*range)) { return f(*range); }
+};
+template <typename F, typename Range>
+auto map (F f, Range range) -> RMap<F,Range> { return { f, range }; }
+
+template <typename F, typename It>
+auto map (F f, It begin, It end) -> decltype(map(f, sequence(begin, end))) {
+    return map(f, sequence(begin, end));
+}
+
+//
+// Filter
+//
+
+template <typename F, typename Range>
+class RFilter {
+    F f;
+    Range range;
+
+    void advanceFilter () { while (range && !f(*range)) { ++range; } }
+public:
+    RFilter (F f, Range range) : f(f), range(range) { info() << "constructed filter()";  advanceFilter(); }
+    operator bool () const { return bool(range); }
+    RFilter& operator++ () { ++range; advanceFilter(); return *this; }
+    auto operator* () const -> decltype(*range) { return *range; }
+};
+template <typename F, typename Range>
+auto filter (F f, Range range) -> RFilter<F,Range> { return { f, range }; }
+
+template <typename F, typename It>
+auto filter (F f, It begin, It end) -> decltype(filter(f, sequence(begin, end))) {
+    return filter(f, sequence(begin, end));
+}
+
+//
+// Reduce
+//
+
+template <typename F, typename Range, typename R>
+auto reduce (F f, Range range, R first) -> R {
+    for (; range; ++range) {
+        first = f(first, *range);
+    }
+    return first;
+}
+template <typename F, typename Range>
+auto reduce (F f, Range range) -> decltype(*range) {
+    if (!range) return {};
+    else {
+        auto first = *range; ++range;
+        for (; range; ++range) {
+            first = f(first, *range);
+        }
+        return first;
+    }
+}
+
+//
+// Join (special case)
+//
+
+template <typename Range>
+std::string join (std::string sep, Range range) {
+    if (!range) return "";
+    else {
+        std::stringstream ss;
+        ss << *range; ++range;
+        for (; range; ++range) {
+            ss << sep << *range;
+        }
+        return ss.str();
+    }
+}
+
+//
+// Take
+//
+
+template <typename Range>
+struct RTake {
+    size_t count;
+    Range range;
+public:
+    RTake (size_t count, Range range) : count(count), range(range) { info() << "constructed take()"; }
+    operator bool () const { return count > 0 && range; }
+    RTake& operator++ () { ++range; --count; return *this; }
+    auto operator* () const -> decltype(*range) { return *range; }
+};
+template <typename Range>
+auto take (size_t n, Range range) -> RTake<Range> {
+    return { n, range };
+}
+
+
+int main () {
+    typedef const std::smatch&  Match;
+    typedef std::string         Key;
+    typedef std::string         Value;
+    
+    auto array = make_hashtable<Key,Value>(std::hash<Key>{}, 8);
+    typedef decltype(array)::KeyValue     KV;
+    typedef decltype(array)::This         Dict;
+
+    writeln();
+    report() << "Starting main program";
+
+    SimpleRegexParser()
+        .caseOf("quit", [&](Match match) {
+            warn() << "exiting";
+            exit(0);
+        })
+        .caseOf(".|list|print|dump", [&](Match match) {
+            if (array.size() == 0) {
+                report() << "[]";
+            } else {
+                static const auto toString = [](const KV& kv) -> std::string { return kv.first + " = " + kv.second; };
+                report() << "[ " << join(", ", map(toString, array.begin(), array.end())) << " ]";
+            }
+        })
+        .caseOf("show", [&](Match match) {
+            report() << array;
+        })
+        .caseOf("length", [&](Match match) {
+            report() << array.size();
+        })
+        .caseOf("memlayout", [&](Match match) {
+            array.debugMemLayout();
+        })
+        .caseOf("fill {} {}", [&](Match match) {
+            auto a = atoi(match[1].str().c_str());
+            auto b = atoi(match[2].str().c_str());
+            if (b < a) { std::swap(a, b); }
+
+            report() << "Filling range " << a << " to " << b;
+            for (; a < b; ++a) {
+                array[std::to_string(a)] = std::to_string(a);
+                report() << array;
+            }
+        })
+        .caseOf("resize {}", [&](Match match) {
+            auto size = atoi(match[1].str().c_str());
+            report() << "Resizing to " << size << " (current size " << array.size() << ")";
+            array.resize(size);
+        })
+        .caseOf("clear", [&](Match match) {
+            array.clear();
+        })
+        .caseOf("del {}", [&](Match match) {
+            auto key = match[1].str();
+            if (array.containsKey(key)) {
+                report() << "deleted " << key;
+                array.deleteKey(key);
+            } else {
+                warn() << "missing key " << key;
+            }
+        })
+        .caseOf("{} = {}", [&](Match match) {
+            auto key = match[1].str(), value = match[2].str();
+            array[key] = value;
+            report() << key << " = " << value;
+        })
+        .caseOf("{}", [&](Match match) {
+            auto key = match[1].str();
+            if (array.containsKey(key)) {
+                report() << array[key];
+            } else {
+                warn() << "undefined";
+            }
+        })
+        .parse(std::cin);
+    return 0;
+}
+
