7b26aa04c4ac296a5d3f6a72318e82d661bdf99d
Seiji Emery <seijiemery@gmail.com>
2017-11-16 16:58:25 -0800

    rewrote parser for DVC10, now solely using HashTbale and w/out perfect hashing algorithm

--- assignment_10/src/DvcSchedule10.cpp
+++ assignment_10/src/DvcSchedule10.cpp
@@ -1,10 +1,18 @@
 // Programmer: Seiji Emery
 // Programmer ID: M00202623
 //
-// DvcSchedule9.cpp
+// DvcSchedule10.cpp
 //
-// Implements a modified DVC parser that displays subjects / sections as a hierarchical tree.
-// Also demonstrates the use of AssociativeArray.
+// Hashtable-based DVC parser, used for both duplicate checking + counting.
+//
+// Note: this is fast, but is not nearly as fast / efficient as my other versions,
+// which were extremely optimized to use eg. perfect hashing (based on characteristics
+// of the data set) and a bitset for duplicate removal; using a hashtable is not
+// nearly as efficient, though it is much more general.
+// 
+// Specifically, this uses ~15x as much memory and runs ~2.5x slower than DVC-4;
+// it is significantly slower than DVC-8, which brought further improvements / optimizations
+// and is highly modular.
 //
 // remote source: https://github.com/SeijiEmery/comp220/blob/master/assignment_09/src/DvcSchedule9.cpp
 //
@@ -65,88 +73,11 @@ LineWriter info   (std::ostream& os = std::cout) { return writeln(os, SET_GREEN)
 #define PACK_STR_4(a,b,c,d) \
     (((uint32_t)d << 24) | ((uint32_t)c << 16) | ((uint32_t)b << 8) | ((uint32_t)a))
 
-// Parses a string that is assumed / required to be a 4-digit integer literal.
-// Faster than atoi - this saved me about 1ms per run (out of ~6ms on 70k files), no joke.
-size_t _4atoi (const char* str) {
-    assert(!(str[0] < '0' || str[0] > '9' ||
-        str[1] < '0' || str[1] > '9' ||
-        str[2] < '0' || str[2] > '9' ||
-        str[3] < '0' || str[3] > '9'));
-    
-    uint32_t s = *((uint32_t*)str);
-    return (size_t)(
-        (((s >> 0) & 0xff) - '0') * 1000 +
-        (((s >> 8) & 0xff) - '0') * 100 +
-        (((s >> 16) & 0xff) - '0') * 10 +
-        (((s >> 24) & 0xff) - '0') * 1
-    );
-}
-void unittest_4atoi () {
-    // std::cout << _4atoi("01234") << '\n';
-    // std::cout << _4atoi("8942") << '\n';
-    assert(_4atoi("01234") == 123);
-    assert(_4atoi("8942") == 8942);
-}
-
-//
-// Parsing algorithm
-//
-
-struct ParseResult {
-    std::string subject;
-    // std::string section;
-    size_t      hash;
-    const char* line;
-
-    friend std::ostream& operator<< (std::ostream& os, ParseResult& result) {
-        return os << result.subject << " (hash " << result.hash << ")";
-    }
-};
-
-bool parse (const char* file, size_t line_num, const char* line, ParseResult& result) {
-    #define require(context, expr) if (!(expr)) { warn(std::cerr) << "PARSING ERROR (" \
-            << context << ", " __FILE__ ":" << __LINE__ << ") in " \
-            << file << ':' << line_num << ", at '" << line << "')"; return false; }
-
-    result.line = line;
-    size_t semester = 0;
-    switch (((uint32_t*)line)[0]) {
-        case PACK_STR_4('S','p','r','i'): require("expected season", (((uint32_t*)line)[1] & 0x00FFFFFF) == PACK_STR_4('n','g',' ','\0')); line += 7; semester = 0; break;
-        case PACK_STR_4('S','u','m','m'): require("expected season", (((uint32_t*)line)[1] & 0x00FFFFFF) == PACK_STR_4('e','r',' ','\0')); line += 7; semester = 1; break;
-        case PACK_STR_4('F','a','l','l'): require("expected season", line[4] == ' ');                                                         line += 5; semester = 2; break;
-        case PACK_STR_4('W','i','n','t'): require("expected season", (((uint32_t*)line)[1] & 0x00FFFFFF) == PACK_STR_4('e','r',' ','\0')); line += 7; semester = 3; break;
-        default: return false;
-    }
-    require("expected year", isnumber(line[0]) && line[4] == '\t');
-    result.hash = semester | (((_4atoi(line) - 2000) & 31) << 2);
-    line += 5;
-
-    require("expected section", isnumber(line[0]) && line[4] == '\t');
-    size_t code = _4atoi(line);
-    result.hash |= (code << 8);
-    line += 5;
-
-    require("expected course", isupper(line[0]));
-    const char* end = strchr(line, '-');
-    require("expected course", end != nullptr && end != line);
-    result.subject = { line, (size_t)(end - line) };
-
-    // require("expected section", *end == '-');
-    // const char* sbegin = end + 1;
-    // const char* send   = strchr(sbegin, '\t');
-    // require("expected section", send != nullptr && send != sbegin);
-    // result.section = { sbegin, (size_t)(send - sbegin) };
-    return true;
-
-    #undef require
-}
-
 //
 // Main program
 //
 
 int main (int argc, const char** argv) {
-    unittest_4atoi();
     std::cout << "Programmer: Seiji Emery\n"
               << "Programmer's id: M00202623\n"
               << "File: " __FILE__ "\n\n";
@@ -171,18 +102,41 @@ int main (int argc, const char** argv) {
         std::cout << "Loaded file '" << path << "'" << std::endl;
     }
 
-    auto duplicateSet = make_hashtable<size_t, bool>(std::hash<size_t>{});
+    auto duplicates   = make_hashtable<std::string, bool>(std::hash<std::string>{});
     auto subjects     = make_hashtable<std::string, size_t>(std::hash<std::string>{});
 
-    // Parse lines
+    // Parse lines 2
     std::string line;
-    ParseResult result;
-    size_t line_num = 0;
+    std::string subject;
     while (getline(file, line)) {
-        if (parse(path, ++line_num, line.c_str(), result) && !duplicateSet.containsKey(result.hash)) {
-            duplicateSet[result.hash] = true;
-            subjects[result.subject] += 1;
+        // Skip duplicate lines
+        if (!duplicates.insert(line, true)) {
+            // warn() << "duplicate line " << line;
+            continue;
         }
+
+        // Filter valid lines only (this is a very unsafe, albeit fast way to do this)
+        const char* s = line.c_str();
+        switch (((uint32_t*)s)[0]) {
+            case PACK_STR_4('S','p','r','i'): break;
+            case PACK_STR_4('S','u','m','m'): break;
+            case PACK_STR_4('F','a','l','l'): break;
+            case PACK_STR_4('W','i','n','t'): break;
+            default: 
+                // warn() << "skipping line " << line; 
+                continue;
+        }
+
+        // Skip season + section fields
+        const char* section = strchr(s, '\t') + 1;    assert(section != s + 1);
+
+        // Get subject string (once again, unsafe / fast)
+        const char* subj = strchr(section, '\t') + 1; assert(subj != section + 1);
+        const char* end  = strchr(subj, '-');         assert(subj != end);
+
+        // Count subject
+        subject.assign(subj, static_cast<size_t>(end - subj));
+        subjects[subject] += 1;
     }
 
     // Fetch + sort results:
@@ -197,9 +151,7 @@ int main (int argc, const char** argv) {
     });
 
     for (const auto& subject : results) {
-        std::cout
-            << subject.first << ", "
-            << subject.second << " section(s)\n";
+        writeln() << subject.first << ", " << subject.second << " section(s)";
     }
     return 0;
 }
