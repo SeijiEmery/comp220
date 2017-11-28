f11d14649274ff019e2a284c3a5fc24b61bd4425
Seiji Emery <seijiemery@gmail.com>
2017-11-28 00:00:17 -0800

    fixed timing code, added basic memory benchmarking

--- assignment_11/src/PriorityQueue.push.cpp
+++ assignment_11/src/PriorityQueue.push.cpp
@@ -28,7 +28,7 @@ public:
     bool valid () const { return started && stopped; }
 
     // Returns total time elapsed, in seconds
-    double elapsed () const { return (double)(startTime - endTime) / CLOCKS_PER_SEC; }
+    double elapsed () const { return (double)(endTime - startTime) / CLOCKS_PER_SEC; }
 
     template <typename F>
     BenchResult& run (const F& inner) {
@@ -82,26 +82,35 @@ public:
         }));
         return static_cast<This&>(*this);
     }
-    void runSuite (std::initializer_list<std::pair<size_t, size_t>> counts) {
+    This& runSuite (std::initializer_list<std::pair<size_t, size_t>> counts) {
         for (const auto& pair : counts) {
             size_t count = pair.first, iterations = pair.second;
             srand(time(nullptr));
             generate(count, [](){ return static_cast<T>(rand() % 2048 - 1024); });
             run(iterations, [&](size_t iterations, double benchDuration /* seconds */) {
-                std::cout << "Ran " << iterations << " iterations, avg " << benchDuration << " for " << count << " element(s)\n";
+                std::cout << "Ran " << iterations << " iterations, total " 
+                    << benchDuration 
+                    << " each " << (benchDuration / count * 1e6) << " µs "
+                    << " for " << count << " element(s)\n";
             });
         }
+        return static_cast<This&>(*this);
     }
 };
 
 template <typename T>
 struct PriorityQueuePushBenchmark : public PriorityQueueBenchmark<T, PriorityQueuePushBenchmark<T>> {
+    T accumulator;
     void run (size_t i, PriorityQueue<T>& queue, const std::vector<T>& data) {
         // std::cout << data.size() << " elements\n";
         for (const auto& element : data) {
             queue.push(element);
         }
+        accumulator += queue.size();
         // std::cout << queue << '\n';
+    }
+    void info () {
+        std::cout << accumulator << '\n';
     }
 };
 
@@ -114,5 +123,64 @@ int main () {
             { 10000, 100 },
             { 100000, 10 },
             { 1000000, 10 },
-        });
+            { 10000000, 1 },
+            // { 100000000, 1 },
+        })
+        .info();
 }
+
+//
+// Memory + time benchmarking code: this hijacks (overloads) global new / delete
+// to trace memory allocations (very simple: # of allocations / frees + # bytes
+// allocated / freed), and adds a global variable that displays this stuff
+// from its dtor (guaranteed to be called after main() but before program exits).
+//
+// It also adds basic time profiling (global ctor / dtor) using std::chrono.
+//
+// All of this can be achieved externally ofc using time + valgrind (*nix),
+// and is perhaps preferable - but implementing these interally was an interesting
+// exercise nevertheless.
+//
+// This can all be disabled if compiling with -D NO_MEM_DEBUG.
+//
+#ifndef NO_MEM_DEBUG
+#include <chrono>
+
+struct MemTracer {
+    void traceAlloc (size_t bytes) { ++numAllocations; allocatedMem += bytes; }
+    void traceFreed (size_t bytes) { ++numFrees; freedMem += bytes;}
+private:
+    size_t numAllocations = 0;  // number of allocations in this program
+    size_t numFrees       = 0;  // number of deallocations in this program
+    size_t allocatedMem   = 0;  // bytes allocated
+    size_t freedMem       = 0;  // bytes freed
+
+    std::chrono::high_resolution_clock::time_point t0;  // time at program start
+public:
+    MemTracer () : t0(std::chrono::high_resolution_clock::now()) {}
+    ~MemTracer () {
+        using namespace std::chrono;
+        auto t1 = high_resolution_clock::now();
+        std::cout << "\nUsed  memory: " << ((double)allocatedMem) * 1e-6 << " MB (" << numAllocations << " allocations)\n";
+        std::cout << "Freed memory: "   << ((double)freedMem)     * 1e-6 << " MB (" << numFrees       << " deallocations)\n";
+        std::cout << "Ran in " << duration_cast<duration<double>>(t1 - t0).count() * 1e3 << " ms\n";
+    }
+} g_memTracer;
+
+void* operator new (size_t size) throw(std::bad_alloc) {
+    g_memTracer.traceAlloc(size);
+    size_t* mem = (size_t*)std::malloc(size + sizeof(size_t));
+    if (!mem) {
+        throw std::bad_alloc();
+    }
+    mem[0] = size;
+    return (void*)(&mem[1]);
+}
+void operator delete (void* mem) throw() {
+    auto ptr = &((size_t*)mem)[-1];
+    g_memTracer.traceFreed(ptr[0]);
+    std::free(ptr);
+}
+
+#endif // NO_MEM_DEBUG
+
