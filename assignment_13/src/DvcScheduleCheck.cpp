452cfccdee5073612513e4ce4241f91e1d2a965a
Seiji Emery <seijiemery@gmail.com>
2017-12-05 14:12:42 -0800

    started assignment 13 (dvc schedule search / check)

---
+++ assignment_13/src/DvcScheduleCheck.cpp
@@ -0,0 +1,23 @@
+// Programmer: Seiji Emery
+// Programmer ID: M00202623
+//
+// DvcScheduleCheck.cpp
+//
+// Loads and searched DVC data for courses with conflicting section numbers
+// (each section number for a given semester is supposed to be unique)
+//
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+using namespace std;
+
+
+
+
+int main (int argc, const char** argv) {
+    unittest_4atoi();
+    std::cout << "Programmer: Seiji Emery\n"
+              << "Programmer's id: M00202623\n"
+              << "File: " __FILE__ "\n\n";
+}
---
+++ assignment_13/src/DvcScheduleSearch.cpp
@@ -0,0 +1,24 @@
+// Programmer: Seiji Emery
+// Programmer ID: M00202623
+//
+// DvcScheduleSearch.cpp
+//
+// Loads DVC and provides an interactive way to search through courses.
+//
+// Uses ANSI text colors, so run this in a terminal (please).
+//
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+using namespace std;
+
+
+
+
+int main (int argc, const char** argv) {
+    unittest_4atoi();
+    std::cout << "Programmer: Seiji Emery\n"
+              << "Programmer's id: M00202623\n"
+              << "File: " __FILE__ "\n\n";
+}
